f2292fd2e5ea1513b1536a36bfbee2bb45915e07
Javier <Javier@Javier-PC.home>
2014-11-19 22:53:32 -0500

    employee stuff works

--- web/js/customerTableButton.js
+++ web/js/customerTableButton.js
@@ -3,4 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+function doSomething()
+{
+    alert("YOOO");  
+   $.ajax({
+       url : 'http://localhost:8080/MatchesFromAbove/getAllCustomers',
+       type : 'GET', 
+       dataType : 'JSON',
+       success : function(data){
+        var something = data[0].ssn;
+        alert(something);
+        }
+    })
+    };
+   
