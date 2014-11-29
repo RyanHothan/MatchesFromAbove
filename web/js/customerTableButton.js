f98d0373401282a6958058c22e9a40f6e391ef80
Acer <Acer@Acer-PC.home>
2014-11-29 15:33:27 -0500

    text change

--- web/js/customerTableButton.js
+++ web/js/customerTableButton.js
@@ -3,11 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-jQuery(function($) {
-    $('title').html('Employee Home');
-});
-
 function populateCustomersTable()
 {
     var displayValue = $("#customersTable").css('display');
